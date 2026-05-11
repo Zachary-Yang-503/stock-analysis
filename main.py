baf8c8ffcff71047ad517901084636f852f6d4d9
Zachary-Yang <jasonyzc503@gmail.com>
2026-05-11 02:49:14 -0400

    update stock analysis

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 print("hello stock project")
+
+import yfinance as yf
+data = yf.download("NVDA TSLA PLTR MSFT", period="6mo")
+print(data.head())
+print(data["Close"])
+print(data["Close"]["NVDA"])
+
+import matplotlib.pyplot as plt
+plt.plot(data["Close"]["NVDA"])
+plt.show()
+
+import pandas
+
+print("ok")
